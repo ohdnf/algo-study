769b7ec03afe51a10b0b6c56fca1b8b33a6299bb
jhj9109 <jhj91_09@naver.com>
2020-11-19 10:21:44 +0900

    Update 우선순위Q 적용

--- python-algorithm-interview/03_non-linear/13_shortest_path_problem/743_network_delay_time/네트워크딜레이타임_장현준.py
+++ python-algorithm-interview/03_non-linear/13_shortest_path_problem/743_network_delay_time/네트워크딜레이타임_장현준.py
@@ -29,3 +29,33 @@ class Solution:
 # Runtime: 548 ms, faster than 30.13% of Python3 online submissions for Network Delay Time.
 # Memory Usage: 15.6 MB, less than 84.04% of Python3 online submissions for Network Delay Time.
 # 위
+
+# 아래
+# Runtime: 488 ms, faster than 47.21% of Python3 online submissions for Network Delay Time.
+# Memory Usage: 16.1 MB, less than 20.68% of Python3 online submissions for Network Delay Time.
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], N: int, K: int) -> int:
+        INF = float('inf')
+        G = dict()
+        for u, v, w in times:
+            if u in G:
+                G[u].append((v, w))
+            else:
+                G[u] = [(v, w)] #노드 0부터 수정
+        
+        import heapq
+        q = []
+        dist = dict()
+        heapq.heappush(q, (0, K))
+        while q:
+            price, node = heapq.heappop(q)
+            if node in dist: continue
+            dist[node] = price
+            if node not in G: continue
+            for w, cost in G[node]:
+                heapq.heappush(q, (price + cost, w))
+        if len(dist) == N:
+            return max(dist.values())
+        else:
+            return -1
