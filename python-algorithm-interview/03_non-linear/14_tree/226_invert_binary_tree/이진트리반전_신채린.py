8c8c3c3471379c27cd10fea758045888512442fa
scl2589 <chaelinshin96@gmail.com>
2020-12-28 00:26:39 +0900

    Add 이진트리반전.py

--- python-algorithm-interview/03_non-linear/14_tree/226_invert_binary_tree/이진트리반전_신채린.py
+++ python-algorithm-interview/03_non-linear/14_tree/226_invert_binary_tree/이진트리반전_신채린.py
@@ -11,3 +11,40 @@ class Solution:
             node.left, node.right = dfs(node.right), dfs(node.left)
             return node   
         return dfs(root)
+# Runtime: 24 ms, faster than 95.03% of Python3 online submissions for Invert Binary Tree.
+# Memory Usage: 14.2 MB, less than 56.46% of Python3 online submissions for Invert Binary Tree.
+
+
+
+# BFS 방식
+def invertTree(self, root: TreeNode) -> TreeNode:
+        queue = collections.deque([root])
+        
+        while queue:
+            node = queue.popleft() 
+            
+            if node:
+                node.left, node.right = node.right, node.left 
+                
+                queue.append(node.left)
+                queue.append(node.right)
+        return root
+
+# Runtime: 32 ms, faster than 56.74% of Python3 online submissions for Invert Binary Tree.
+# Memory Usage: 14.3 MB, less than 29.68% of Python3 online submissions for Invert Binary Tree.
+
+#DFS 방식
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        stack = collections.deque([root])
+        
+        while stack:
+            node = stack.pop()
+            if node:
+                node.left, node.right = node.right, node.left 
+                
+                stack.append(node.left)
+                stack.append(node.right)
+        return root
+
+Runtime: 28 ms, faster than 82.35% of Python3 online submissions for Invert Binary Tree.
+Memory Usage: 14.2 MB, less than 56.46% of Python3 online submissions for Invert Binary Tree.
