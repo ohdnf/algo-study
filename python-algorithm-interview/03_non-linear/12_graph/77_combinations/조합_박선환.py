15d974e09ee0ed3ef114dc494f08290a5ec234c6
SunHwan-Park <57381062+SunHwan-Park@users.noreply.github.com>
2021-01-08 19:32:58 +0900

    Add 조합_박선환.py

---
+++ python-algorithm-interview/03_non-linear/12_graph/77_combinations/조합_박선환.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        nums = list(range(1, n+1))
+        result = []
+        
+        def dfs(i, current=[]):
+            if len(current) == k:
+                result.append(set(current))
+                return
+            if k + i > len(current) + n:
+                return
+            dfs(i+1, current + [nums[i]])
+            dfs(i+1, current)
+        dfs(0)
+        
+        return result
