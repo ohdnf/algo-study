90788b284bcb5d51f8c6fe4a628d0f450a144b43
ohdnf <jupyohong7@gmail.com>
2021-02-16 00:09:54 +0900

    Update 리스트정렬_홍주표.py

--- python-algorithm-interview/04_algorithm/01_sort/148_sort_list/리스트정렬_홍주표.py
+++ python-algorithm-interview/04_algorithm/01_sort/148_sort_list/리스트정렬_홍주표.py
@@ -3,26 +3,51 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def sortList(self, head: ListNode) -> ListNode:
-        curr = head
-        if curr is None:
-            return curr
+# class Solution:
+#     def sortList(self, head: ListNode) -> ListNode:
+#         curr = head
+#         if curr is None:
+#             return curr
 
-        mem = None
-        max_val = float('-inf')
-        while curr.next:
-            if mem == head:
-                break
+#         mem = None
+#         max_val = float('-inf')
+#         while curr.next:
+#             if mem == head:
+#                 break
 
-            if curr.val > curr.next.val:
-                curr.val, curr.next.val = curr.next.val, curr.val
-            max_val = max(max_val, curr.val, curr.next.val)
-            curr = curr.next
+#             if curr.val > curr.next.val:
+#                 curr.val, curr.next.val = curr.next.val, curr.val
+#             max_val = max(max_val, curr.val, curr.next.val)
+#             curr = curr.next
             
-            if curr.next is None or curr == mem:
-                if curr.val == max_val:
-                    mem = curr
-                    curr = head
+#             if curr.next is None or curr == mem:
+#                 if curr.val == max_val:
+#                     mem = curr
+#                     curr = head
 
-        return head
+#         return head
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if l1 and l2:
+            if l1.val > l2.val:
+                l1, l2 = l2, l1
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            
+        return l1 or l2
+    
+    def sortList(self, head: ListNode) -> ListNode:
+        if not (head and head.next):
+            return head
+        
+        half, slow, fast = None, head, head
+        while fast and fast.next:
+            half, slow, fast = slow, slow.next, fast.next.next
+        half.next = None
+        
+        l1 = self.sortList(head)
+        l2 = self.sortList(slow)
+        
+        return self.mergeTwoLists(l1, l2)
+
+
