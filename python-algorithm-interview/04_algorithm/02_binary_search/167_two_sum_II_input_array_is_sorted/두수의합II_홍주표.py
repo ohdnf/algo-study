b32db5e96a09a6237750d1485902219f02b73393
Jupyo Hong <jupyohong7@gmail.com>
2021-04-15 23:45:44 +0900

    Add 두수의합합II_홍주표.py

---
+++ python-algorithm-interview/04_algorithm/02_binary_search/167_two_sum_II_input_array_is_sorted/두수의합II_홍주표.py
@@ -0,0 +1,25 @@
+# class Solution:
+#     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+#         for i in range(len(numbers) - 1):
+#             if numbers[i] + numbers[-1] < target:
+#                 continue
+#             for j in range(i + 1, len(numbers)):
+#                 if numbers[i] + numbers[j] == target:
+#                     return [i + 1, j + 1]
+
+from typing import List
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for idx in range(len(numbers) - 1):
+            temp = target - numbers[idx]
+            left = idx + 1
+            right = len(numbers) - 1
+            while left <= right:
+                mid = (left + right) // 2
+                if numbers[mid] < temp:
+                    left = mid + 1
+                elif numbers[mid] > temp:
+                    right = mid - 1
+                else:
+                    return [idx + 1, mid + 1]
