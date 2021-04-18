4114e4ad02c48e624d50658d78dc0ef891c5f264
Jupyo Hong <jupyohong7@gmail.com>
2021-04-18 20:15:55 +0900

    Update 메뉴리뉴얼_홍주표.py

--- programmers/2021_kakao_blind_recruit/메뉴리뉴얼/메뉴리뉴얼_홍주표.py
+++ programmers/2021_kakao_blind_recruit/메뉴리뉴얼/메뉴리뉴얼_홍주표.py
@@ -2,6 +2,52 @@ from collections import defaultdict, Counter
 from itertools import combinations
 
 def solution(orders, course):
+    course_count = Counter()
+    for order in orders:
+        for length in course:
+            for combi in combinations(order, length):
+                course_count[''.join(sorted(combi))] += 1
+    course_length = defaultdict(list)
+    for menu, count in course_count.items():
+        course_length[len(menu)].append((menu, count))
+    result = []
+    for count, courses in course_length.items():
+        courses.sort(key=lambda c: (-c[1], c[0]))
+        max_course_length = courses[0][1]
+        for course, count in courses:
+            if count == max_course_length and count > 1:
+                result.append(course)
+    return sorted(result)
+
+"""
+정확성  테스트
+테스트 1 〉	통과 (0.27ms, 10.4MB)
+테스트 2 〉	통과 (0.16ms, 10.4MB)
+테스트 3 〉	통과 (0.28ms, 10.2MB)
+테스트 4 〉	통과 (0.26ms, 10.3MB)
+테스트 5 〉	통과 (0.22ms, 10.3MB)
+테스트 6 〉	통과 (0.60ms, 10.2MB)
+테스트 7 〉	통과 (0.65ms, 10.1MB)
+테스트 8 〉	통과 (4.81ms, 10.3MB)
+테스트 9 〉	통과 (3.32ms, 10.3MB)
+테스트 10 〉	통과 (5.73ms, 10.8MB)
+테스트 11 〉	통과 (3.37ms, 10.5MB)
+테스트 12 〉	통과 (3.81ms, 10.7MB)
+테스트 13 〉	통과 (5.51ms, 10.7MB)
+테스트 14 〉	통과 (3.68ms, 10.6MB)
+테스트 15 〉	통과 (5.25ms, 10.7MB)
+테스트 16 〉	통과 (3.27ms, 10.4MB)
+테스트 17 〉	통과 (1.00ms, 10.2MB)
+테스트 18 〉	통과 (0.38ms, 10.2MB)
+테스트 19 〉	통과 (0.05ms, 10.3MB)
+테스트 20 〉	통과 (1.08ms, 10.2MB)
+"""
+
+
+from collections import defaultdict, Counter
+from itertools import combinations
+
+def old_solution(orders, course):
     menus = Counter()
     for order in orders:
         for length in range(2, len(order) + 1):
@@ -19,7 +65,6 @@ def solution(orders, course):
             if count == max_order_count and count > 1:
                 result.append(course)
     return sorted(result)
-
 
 """
 정확성  테스트
