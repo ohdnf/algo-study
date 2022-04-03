6287a28071ecd37696eb2435e742d2db1af0ce19
Jupyo Hong <jupyohong7@gmail.com>
2022-04-03 23:11:33 +0900

    주유소 교재 답안 추가

--- python-algorithm-interview/04_algorithm/05_greedy_algorithm/134_gas_station/주유소_홍주표.py
+++ python-algorithm-interview/04_algorithm/05_greedy_algorithm/134_gas_station/주유소_홍주표.py
@@ -20,6 +20,22 @@ class Solution:
         return output
 
 
+class SolutionBook:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        if sum(gas) < sum(cost):
+            return -1
+        start, fuel = 0, 0
+        for i in range(len(gas)):
+            if gas[i] + fuel < cost[i]:
+                # 다음 주유소로 갈 수 없는 경우
+                # 출발점을 다음 주유소로 이동
+                start = i + 1
+                fuel = 0
+            else:
+                fuel += gas[i] - cost[i]
+        return start
+
+
 if __name__ == "__main__":
     print(Solution().canCompleteCircuit([1, 2, 3, 4, 5], [3, 4, 5, 1, 2]))
     print(Solution().canCompleteCircuit([2, 3, 4], [3, 4, 3]))
